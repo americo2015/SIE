41109baaa6635a226eebb4872e4a7ad606bac5bb
Americo <Americo@DESKTOP-H5I9IG3>
2017-10-23 14:19:50 +0200

    SIE Övning utan unit test

---
+++ SIE/Sie.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace SIE
+{
+    class Sie
+    {
+        static void Main(string[] args)
+        {
+            var streamReader = File.OpenText(@"SIE.txt");
+            var rows = 0;
+            var accounts = new Dictionary<string, decimal>();
+
+            while (true)
+            {
+                var line = streamReader.ReadLine();
+                if (line == null)
+                    break;
+
+                string pattern = @"(#TRANS) (\d{4}) {.*} (-?\d*.\d*)";
+                var match = Regex.Match(line, pattern);
+                if (match.Success)
+                {
+                    rows++;
+
+                    var accountId = match.Groups[2].Value;
+                    var amount = decimal.Parse(match.Groups[3].Value, CultureInfo.InvariantCulture);
+
+                    if (accounts.ContainsKey(accountId))
+                        accounts[accountId] += amount;
+                    else
+                        accounts[accountId] = amount;
+                }
+                    
+            }
+
+
+            foreach (var entry in accounts.OrderBy(e => e.Key))
+                Console.WriteLine($"{entry.Value.ToString("F2")}");
+
+            Console.WriteLine("Total amount: " + accounts.Sum(entry => entry.Value));
+            Console.WriteLine($"Total rows: {rows}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SIETest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SIETest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SIETest")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2ed695e0-c0e2-493e-ae8e-a268d5d27b08")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
